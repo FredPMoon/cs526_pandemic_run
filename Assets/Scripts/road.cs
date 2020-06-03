6d4a437e49cd883f3fd2c335c67e74c89db32a32
Anyang Yu <yuanyang872@outlook.com>
2020-06-03 09:43:09 +0800

    original

---
+++ Assets/Scripts/road.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class road : MonoBehaviour
+{
+    private GameObject main_camera;
+    void Start()
+    {
+        main_camera = GameObject.FindGameObjectWithTag("MainCamera");
+        //Debug.Log(main_camera.transform.position.z);
+    }
+
+    void Update()
+    {
+        //Debug.Log(main_camera.transform.position.z);
+        if (main_camera.transform.position.z > transform.position.z){
+            //Debug.Log("pass"+transform.position.z);
+            roadManage._instance.GenerateRoad();
+            Destroy(this.gameObject);
+        }
+
+    }
+}
