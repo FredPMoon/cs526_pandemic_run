3bae9277d8d3eb74066bac7e670e6b1c21910441
choly <879325103@qq.com>
2020-06-16 13:06:21 -0700

    complete assets

--- Assets/Scripts/ObstacleControl.cs
+++ Assets/Scripts/ObstacleControl.cs
@@ -5,86 +5,102 @@ using UnityEngine;
 public class ObstacleControl : MonoBehaviour
 {
     public GameObject[] Obstacles;
-    // public Transform player;
+    //public GameObject player;
     List<int> Zindex = new List<int>(); // record existing obstacles
-
-    private void Awake()
-    {
-        //player = GameObject.FindGameObjectsWithTag("player").transform;
-    }
 
     // Start is called before the first frame update
     void Start()
     {
+        //player = GameObject.FindGameObjectWithTag("player");
         Create_Obstacle();
     }
 
     public void Create_Obstacle()
     {
-        float MinLength = 3; // distance between two obstacles
-        float MaxLength = 9;
-        float StartLength = 1; // obstacle start position
-
-        float startz = transform.position.z;
-        float endz = startz + 200;
+        float StartLength = 15; // obstacle start position 
         float z = transform.position.z + StartLength;
 
-        float x = transform.position.x;
+        int prob = Random.Range(0, 100);// Obstacles.Length
+        int obsIndex = 0;
 
-       
-        z += Random.Range(MinLength, MaxLength);
-
-        if (z > endz)
+        if (prob <= 85)
         {
-            //break;
+            obsIndex = 0;
+        }
+        else if (prob <= 90)
+        {
+            obsIndex = 1;
+        }
+        else if (prob <= 95)
+        {
+            obsIndex = 2;
         }
         else
         {
-            int obsIndex = Random.Range(0, Obstacles.Length);
-            //print(obsIndex);
-         
-            Vector3 position = GetPos(x,z,Zindex);
-            //Debug.Log(position);
+            obsIndex = 3;
+        }
+
+        Vector3 position = GetPos(z, Zindex);
+        Debug.Log("position: " + position + "obsIndex: " + obsIndex);
+
+        if (position.z != 0)
+        {
             GameObject obj = GameObject.Instantiate(Obstacles[obsIndex], position, Quaternion.identity);
-            if (position.x == -4){
-                obj.tag = "1";
-            }
-            else if (position.x == 0)
+            if (obsIndex == 0)
             {
-                obj.tag = "2";
+                if (position.x == -4)
+                {
+                    obj.tag = "1";
+                }
+                else if (position.x == 0)
+                {
+                    obj.tag = "2";
+                }
+                else
+                {
+                    obj.tag = "3";
+                }
+            }
+            else if (obsIndex == 1)
+            {
+                obj.tag = "4"; // meterial 1
+            }
+            else if (obsIndex == 2)
+            {
+                obj.tag = "5"; // meterial 2
             }
             else
             {
-                obj.tag = "3";
+                obj.tag = "6"; // meterial 3
             }
-            
         }
-}
+    }
 
-    Vector3 GetPos(float x, float z, List<int> Zindex)
+    Vector3 GetPos(float z, List<int> Zindex)
     { 
 
-        int Xindex = Random.Range(-1, 2) * 4;
+        int Xindex = Random.Range(-1, 2) * 3; //-4，0，4： three tracks
 
         int z_int = (int) Mathf.Round(z);
 
         if (Zindex.Contains(z_int) == true)
         {
             z_int = 0;
-            Debug.Log(z);
+            // Debug.Log("z: " + z);
         }
         else
         {
-            for (int i = (int)z_int - 3; i <= z_int + 3; i++)
+            for (int i = (int)z_int; i <= z_int + 3; i++)
             {
-                Zindex.Add(z_int);
+                Zindex.Add(i);
             }
-            //Debug.Log("list_length = " + Zindex.ToArray().Length);
-            Debug.Log(z_int);
+            // Debug.Log("list_length = " + Zindex.ToArray().Length);
+            // Debug.Log(z_int);
         }
 
-        return new Vector3(Xindex, 0, z_int);
+        return new Vector3(Xindex, 1, z_int);
     }
+
     // Update is called once per frame
     void Update()
     {
