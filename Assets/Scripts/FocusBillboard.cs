d9b5244497ea2594b333d66f75b0266464054cf0
Anyang Yu <yuanyang872@outlook.com>
2020-07-08 09:44:12 +0800

    Revert "Merge branch 'master' of https://github.com/FredPMoon/cs526_pandemic_run"
    This reverts commit 5d4a8ceaee13678bdbae4143c3c23f0330f04f25, reversing
    changes made to b7b9cf6d6dde841f558342104e4212bed0b8a408.

---
+++ Assets/Scripts/FocusBillboard.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusBillboard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private TextMesh textMesh;
+    private Transform canvas;
+
+    public static FocusBillboard current;
+    void Start()
+    {
+        current = transform.GetComponent<FocusBillboard>();
+        canvas = gameObject.transform.Find("Canvas");
+        //textMesh = canvas.Find("Text").GetComponent<TextMesh>();
+        HideBillboard();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void ShowBillboard(Vector3 position, Vector3 rulerAngles, string text)
+    {
+        Debug.Log("bb position: " + position);
+        canvas.gameObject.SetActive(true);
+        transform.position = position;
+        transform.eulerAngles = new Vector3(0, rulerAngles.y, 0);
+    }
+
+    public void HideBillboard()
+    {
+        canvas.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/recipe.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recipe : MonoBehaviour
+{
+    static string elements = "abcde";
+    char[] ingredients = elements.ToCharArray();
+
+    public GameObject recipe_1;
+    public GameObject recipe_2;
+    public GameObject recipe_3;
+    string[] recipes00 = {"aacx", "bbax", "abbx", "abbx"};
+    string[] recipes01 = {"abcy", "bacy", "caby", "acby"};
+    string[] recipes02 = {"bbcaz", "cbaaz", "aacbz", "bccaz"};
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void powerUp_x()
+    {
+        //adding health
+        Debug.Log("X");
+    }
+
+    void powerUp_y()
+    {
+        //slow the camera move speed
+        Debug.Log("Y");
+    }
+
+    void powerUp_z()
+    {
+        //clear the infected, positive patients
+        Debug.Log("Z");
+    }
+    
+}
